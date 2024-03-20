9ea6e1bcb0bf20647f4d5e9f3ceb77605e19f1d3
luffyhaoshoku <luffy3day2years@gmail.com>
2024-03-20 15:39:55 +0000

    adding all data types

--- course/Hello.java
+++ course/Hello.java
@@ -1,8 +1,19 @@
 class Hello
 {
     public static void main(String a[]) 
-    {
-        System.out.println("Hello, World");
+    {   
+        int num1 = 9;
+        //byte by = 129; not within range 
+        byte by = 126;
+        short sh = 558;
+        long l = 58554l;
+
+        float f = 5.8f;
+        double d = 5.8;
+
+        boolean b = true;
+
+        
     }
 }
     
